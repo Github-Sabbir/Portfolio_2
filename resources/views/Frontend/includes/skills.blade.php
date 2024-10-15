4790e3d3296d1ee6fe0f8076067c208b3642af1a
Github-Sabbir <mdabdulalimsabbir000@gmail.com>
2024-10-15 23:58:15 +0600

    skill item time fast

--- resources/views/Frontend/includes/skills.blade.php
+++ resources/views/Frontend/includes/skills.blade.php
@@ -17,119 +17,119 @@
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="250">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="150">
         <div class="features-item">
         <i class="bi bi-filetype-css"style="color: #6EACDA;"></i>
           <h3 class="stretched-link">CSS&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="400">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="200">
         <div class="features-item">
         <i class="bi bi-bootstrap"style="color: #433D8B;"></i>
           <h3 class="stretched-link">Bootstrap&nbsp;<sub>( Mid )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="550">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="250">
         <div class="features-item">
         <i class="bi bi-filetype-js"style="color: #F4CE14;"></i>
           <h3 class="stretched-link">Javascript&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="700">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="300">
         <div class="features-item">
         <i class="bi bi-git"style="color: #EC8305;"></i>
           <h3 class="stretched-link">Git&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="850">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="350">
         <div class="features-item">
         <i class="bi bi-github"style="color: #40534C;"></i>
           <h3 class="stretched-link">Github&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1000">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="400">
         <div class="features-item">
         <i class="bi bi-filetype-php"style="color: #5A72A0;"></i>
           <h3 class="stretched-link">Php&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1150">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="450">
         <div class="features-item">
         <i class="bi bi-braces"style="color: #C80036;"></i>
           <h3 class="stretched-link">Laravel&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1300">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="500">
         <div class="features-item">
         <i class="bi bi-terminal"style="color: #6EACDA;"></i>
           <h3 class="stretched-link">VS code&nbsp;<sub>( Mid )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1450">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="550">
         <div class="features-item">
         <i class="bi bi-feather"style="color: #FF4191;"></i>
           <h3 class="stretched-link">Phpstrom&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
       
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1600">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="600">
         <div class="features-item">
           <i class="bi bi-vector-pen"style="color: #FF8225;"></i>
           <h3 class="stretched-link">Ilustrator&nbsp;<sub>( Mid )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1750">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="650">
         <div class="features-item">
         <i class="bi bi-pencil"style="color: #76ABAE;"></i>
           <h3 class="stretched-link">Photoshop&nbsp;<sub>( Junior )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="1900">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="700">
         <div class="features-item">
         <i class="bi bi-browser-chrome"style="color: #18d26e;"></i>
           <h3 class="stretched-link">Chrome&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="2050">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="750">
         <div class="features-item">
         <i class="bi bi-browser-edge"style="color: #03346E;"></i>
           <h3 class="stretched-link">Edge&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
       
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="2200">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="800">
         <div class="features-item">
           <i class="bi bi-browser-firefox"style="color: #F57D1F;"></i>
           <h3 class="stretched-link">Firefox&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
       
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="2350">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="850">
         <div class="features-item">
         <i class="bi bi-alexa"style="color: #A91D3A;"></i>
           <h3 class="stretched-link">Opera&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="2500">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="900">
         <div class="features-item">
         <i class="bi bi-windows"style="color: #0E46A3;"></i>
           <h3 class="stretched-link">Window&nbsp;<sub>( Expert )</sub></h3>
         </div>
       </div><!-- End Feature Item -->
 
-      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="2650">
+      <div class="col-lg-3 col-md-4" data-aos="fade-up" data-aos-delay="950">
         <div class="features-item">
         <i class="bi bi-android2"style="color: #18d26e;"></i>
           <h3 class="stretched-link">Android&nbsp;<sub>( Expert )</sub></h3>
