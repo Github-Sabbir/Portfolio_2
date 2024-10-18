9fab8bfb6a53a4d713519d56db4268633e44b380
Github-Sabbir <mdabdulalimsabbir000@gmail.com>
2024-10-19 03:25:48 +0600

    remove all

--- resources/views/Frontend/includes/css.blade.php
+++
@@ -1,9 +0,0 @@
-  <!-- Vendor CSS Files -->
-  <link href="{{ asset('/') }}assets/Frontend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="{{ asset('/') }}assets/Frontend/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="{{ asset('/') }}assets/Frontend/vendor/aos/aos.css" rel="stylesheet">
-  <link href="{{ asset('/') }}assets/Frontend/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link href="{{ asset('/') }}assets/Frontend/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
-
-  <!-- Main CSS File -->
-  <link href="{{ asset('/') }}assets/Frontend/css/main.css" rel="stylesheet">
--- resources/views/Frontend/includes/meta.blade.php
+++
@@ -1,4 +0,0 @@
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
-<meta name="description" content="">
-<meta name="keywords" content="web, web design, web Designer, web Developer, Md Abdul Alim, Portfolio">
